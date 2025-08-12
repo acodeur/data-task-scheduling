08cb898937ed98b562f49b89c9bb69785bac70fb
acodeur <ccyzhope@126.com>
2025-08-12 20:58:33 +0800

    update .gitignore

---
+++ task-scheduling-server/src/main/resources/webapp/src/views/Dashboard.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="dashboard">
+    <h2>dashboard</h2>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style lang="less" scoped>
+.dashboard {
+  padding: 5px;
+  background-color: #f0f0f0;
+  border-radius: 8px;
+}
+</style>
